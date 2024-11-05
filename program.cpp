95399fffffa6415f04414102673e005658fe221a
sczur <szczur@mat.umk.pl>
2024-11-05 15:11:29 +0100

    dodano silnia(int)

--- program.cpp
+++ program.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int silnia(int l) {
+	int s = 1;
+	for (int i = 2; i <= l; i++) {
+		s *= i;
+	}
+	return s;
+}
+
 int main() {
 	int x;
 	cin >> x;
-	cout << x;
+	cout << silnia(x);
 	return 0;
 }
